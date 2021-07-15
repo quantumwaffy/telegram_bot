cb1174a52bfd08bfcbf8703ca51b3569c9bc65c9
Aleksandr Chistyh <sashchystyh@yandex.ru>
2021-07-15 10:31:44 +0300

    fix: dependencies

--- keep_alive.py
+++ keep_alive.py
@@ -4,18 +4,21 @@ import logging
 import requests
 
 app = Flask('')
-@app.route('/')
 
+
+@app.route('/')
 def home():
   return "Hello, I am alive!"
 
+
 def run():
-  app.run(host = '0.0.0.0', port = 8080)
+  app.run(host='0.0.0.0', port=8080)
+
 
 def make_keep_alive():
   logging.basicConfig(level=logging.DEBUG)
   s = requests.Session()
   r = s.get("http://httpbin.org/cookies")
   print(r.text)
-  t = Thread(target = run)
+  t = Thread(target=run)
   t.start()
